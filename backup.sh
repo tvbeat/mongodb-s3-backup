f3bd2e3fefd3ba08601331e32c1d59da14d5eecb
ksinko <raeonz@gmail.com>
2015-05-28 16:31:52 +0200

    Added specific db dump, and bucket folder support

--- backup.sh
+++ backup.sh
@@ -14,27 +14,36 @@ usage()
 cat << EOF
 usage: $0 options
 
-This script dumps the current mongo database, tars it, then sends it to an Amazon S3 bucket.
+This script dumps the mongo database, tars it, then sends it to an Amazon S3 bucket.
 
 OPTIONS:
    -h      Show this message
-   -u      Mongodb user
-   -p      Mongodb password
+
+MANDATORY:
    -k      AWS Access Key
    -s      AWS Secret Key
    -r      Amazon S3 region
    -b      Amazon S3 bucket name
+
+OPTIONAL:
+   -u      MongoDB user
+   -p      MongoDB password
+   -d      Specific database to backup
+   -f      Bucket folder
+
 EOF
 }
 
 MONGODB_USER=
 MONGODB_PASSWORD=
+DATABASE=
 AWS_ACCESS_KEY=
 AWS_SECRET_KEY=
 S3_REGION=
 S3_BUCKET=
+FOLDER=
 
-while getopts “ht:u:p:k:s:r:b:” OPTION
+while getopts “ht:u:p:d:k:s:r:b:f:” OPTION
 do
   case $OPTION in
     h)
@@ -46,6 +55,9 @@ do
       ;;
     p)
       MONGODB_PASSWORD=$OPTARG
+      ;;
+    d)
+      DATABASE=$OPTARG
       ;;
     k)
       AWS_ACCESS_KEY=$OPTARG
@@ -59,49 +71,75 @@ do
     b)
       S3_BUCKET=$OPTARG
       ;;
+    f)
+      FOLDER=$OPTARG
+      ;;
     ?)
       usage
       exit
-    ;;
+      ;;
   esac
 done
 
-if [[ -z $MONGODB_USER ]] || [[ -z $MONGODB_PASSWORD ]] || [[ -z $AWS_ACCESS_KEY ]] || [[ -z $AWS_SECRET_KEY ]] || [[ -z $S3_REGION ]] || [[ -z $S3_BUCKET ]]
+if [[ -z $AWS_ACCESS_KEY ]] || [[ -z $AWS_SECRET_KEY ]] || [[ -z $S3_REGION ]] || [[ -z $S3_BUCKET ]]
 then
   usage
   exit 1
 fi
 
 # Get the directory the script is being run from
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
+DIR="/tmp/mongodb_backup"
 # Store the current date in YYYY-mm-DD-HHMMSS
 DATE=$(date -u "+%F-%H%M%S")
-FILE_NAME="backup-$DATE"
-ARCHIVE_NAME="$FILE_NAME.tar.gz"
 
 # Lock the database
 # Note there is a bug in mongo 2.2.0 where you must touch all the databases before you run mongodump
-#mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
+#mongo  admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
 
 # Dump the database
-mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/backup/$FILE_NAME
+
+if [[ ! -z $DATABASE ]]
+then
+   FILE_NAME="$DATABASE-$DATE"
+   if [[ ! -z $MONGODB_USER ]] && [[ ! -z $MONGODB_PASSWORD ]]
+   then
+      mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" -db "$DATABASE" --out $DIR/$FILE_NAME
+   else
+      mongodump -db "$DATABASE" --out $DIR/$FILE_NAME
+   fi
+else
+   FILE_NAME="$DATE"
+   if [[ ! -z $MONGODB_USER ]] && [[ ! -z $MONGODB_PASSWORD ]]
+   then
+      mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/$FILE_NAME
+   else
+      mongodump --out $DIR/$FILE_NAME
+   fi
+fi
+
+ARCHIVE_NAME="$FILE_NAME.tar.gz"
 
 # Unlock the database
-#mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "printjson(db.fsyncUnlock());"
+#mongo admin --eval "printjson(db.fsyncUnlock());"
 
 # Tar Gzip the file
-tar -C $DIR/backup/ -zcvf $DIR/backup/$ARCHIVE_NAME $FILE_NAME/
-
+tar -C $DIR/ -zcvf $DIR/$ARCHIVE_NAME $FILE_NAME/
 # Remove the backup directory
-rm -r $DIR/backup/$FILE_NAME
+rm -r $DIR/$FILE_NAME
 
 # Send the file to the backup drive or S3
 
-cd $DIR/backup
+cd $DIR
 
 hmac="openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:"
 hexdump="xxd -p -c 256"
+
+PAYLOAD="$ARCHIVE_NAME"
+
+if [[ ! -z $FOLDER ]]
+then
+ PAYLOAD="$FOLDER/$ARCHIVE_NAME"
+fi
 
 HEADERS="date;host;x-amz-acl;x-amz-content-sha256;x-amz-date"
 
@@ -109,30 +147,29 @@ DATE_ISO=$(date -u "+%Y%m%dT%H%M%SZ")
 DATE_SHORT=$(date -u "+%Y%m%d")
 DATE_HEADER=$(date -u "+%a, %d %h %Y %T %Z")
 
+
 AWS_HMAC=$(echo -e -n AWS4$AWS_SECRET_KEY | $hexdump)
 DATE_HMAC=$(echo -e -n $DATE_SHORT | $hmac$AWS_HMAC | $hexdump)
 REGION_HMAC=$(echo -e -n $S3_REGION | $hmac$DATE_HMAC | $hexdump)
 SERVICE_HMAC=$(echo -e -n "s3" | $hmac$REGION_HMAC | $hexdump)
 SIGKEY=$(echo -e -n "aws4_request" | $hmac$SERVICE_HMAC | $hexdump)
 
+
 FILE_HASH=$(shasum -ba 256 "$ARCHIVE_NAME" | awk '{ print $1 }')
-CANONICAL_REQ="echo -e -n "PUT\\n/$ARCHIVE_NAME\\n\\ndate:$DATE_HEADER\\nhost:$S3_BUCKET.s3.amazonaws.com\\nx-amz-acl:public-read\\nx-amz-content-sha256:$FILE_HASH\\nx-amz-date:$DATE_ISO\\n\\n$HEADERS\\n$FILE_HASH""
+CANONICAL_REQ="echo -e -n "PUT\\n/$PAYLOAD\\n\\ndate:$DATE_HEADER\\nhost:$S3_BUCKET.s3.amazonaws.com\\nx-amz-acl:public-read\\nx-amz-content-sha256:$FILE_HASH\\nx-amz-date:$DATE_ISO\\n\\n$HEADERS\\n$FILE_HASH""
 CANONICAL_REQ_HASH=$($CANONICAL_REQ | shasum -a 256 | awk '{ print $1 }')
 SIGN="echo -e -n "AWS4-HMAC-SHA256\\n$DATE_ISO\\n$DATE_SHORT/$S3_REGION/s3/aws4_request\\n$CANONICAL_REQ_HASH""
 SIGNATURE=$($SIGN | $hmac$SIGKEY | $hexdump )
 
-echo -e -n "AWS HTTP Response code: "
-
 curl -s -o /dev/null -w "%{http_code}" \
-  -T "$ARCHIVE_NAME" \
+  -T "$DIR/$ARCHIVE_NAME" \
   -H "Authorization: AWS4-HMAC-SHA256 Credential=$AWS_ACCESS_KEY/$DATE_SHORT/$S3_REGION/s3/aws4_request,SignedHeaders=$HEADERS,Signature=$SIGNATURE" \
   -H "Date: $DATE_HEADER" \
   -H "x-amz-acl: public-read" \
   -H "x-amz-content-sha256: $FILE_HASH" \
   -H "x-amz-date: $DATE_ISO" \
-  "https://$S3_BUCKET.s3.amazonaws.com/$ARCHIVE_NAME"
-
-echo ""
+  "https://$S3_BUCKET.s3.amazonaws.com/$PAYLOAD"
 
 # Remove the tarball
+echo ""
 rm $ARCHIVE_NAME
